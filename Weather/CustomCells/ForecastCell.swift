b22f408cec55fb5da794901f09be597b67e79f0d
Venkat Kukunuru <venkatkukunuru@gmail.com>
2019-05-23 09:40:55 -0400

    create custom cells for table view and collection view

---
+++ Weather/CustomCells/ForecastCell.swift
@@ -0,0 +1,18 @@
+//
+//  ForecastCell.swift
+//  Weather
+//
+//  Created by venkat kukunuru on 5/22/19.
+//  Copyright © 2019 venkat kukunuru. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ForecastCell: UICollectionViewCell {
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var iconImage: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+}
